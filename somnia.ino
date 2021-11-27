0f27a55553164c8367f0b9c9cd0db0a458e31fac
fedornabilkin <fedornabilkin@yandex.ru>
2021-11-27 17:13:50 +0300

    Update somnia.ino
    Add mode/

--- somnia.ino
+++ somnia.ino
@@ -2,11 +2,20 @@
 int LED_RED = 9;
 int LED_BLUE = 10;
 int LED_GREEN = 11;
+int LED_DEFAULT = 13;
 
 int looper = 0;
-int sleep = 10;
-int modeTime = 19000;
-int rgbTimes[] = {4000, 7000, 8000};
+int sleep = 0;
+int mode = 3;
+// green, red, blue, loop
+int rgbTimes[][5] = {
+  {4000, 7000, 8000, 0, 10}, // 4-7-8
+  {5000, 0, 5000, 0, 8}, // 5-5
+  {2000, 0, 4000, 0, 15}, // 2-4
+  {4000, 4000, 4000, 4000, 5}, // 4-4-4-4 square
+};
+
+
 
 // setup
 void setup()
@@ -14,29 +23,36 @@ void setup()
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode(LED_DEFAULT, OUTPUT);
+  digitalWrite(LED_DEFAULT, LOW);
 }
 
 // loop
 void loop()
 {
   looper += 1;
+  sleep = rgbTimes[mode][4];
   
   if (looper >= sleep) {
-    flash(13, 4000);
+    //analogWrite(LED_BLUE, 255/4);
     return;
   }
 
   
-  flash(LED_GREEN, rgbTimes[0]);
-  flash(LED_RED, rgbTimes[1]);
-  flash(LED_BLUE, rgbTimes[2]);
+  flash(LED_GREEN, rgbTimes[mode][0]);
+  flash(LED_RED, rgbTimes[mode][1]);
+  flash(LED_BLUE, rgbTimes[mode][2]);
+  flash(LED_RED, rgbTimes[mode][3]);
   
 }
 
 // other
 void flash(int pin, int ledTime)
 {
+  if(ledTime < 2000){
+    return;
+  }
+  
   pinMode(pin, OUTPUT);
 
   int d = 250;
